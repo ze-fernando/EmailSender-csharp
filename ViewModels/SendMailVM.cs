dabafbda6802d8315abb71f159f3e1ba8ffb3f83
ze-fernando <jfenginner935@gmail.com>
2023-10-23 16:08:01 -0300

    create ViewModel

---
+++ ViewModels/SendMailVM.cs
@@ -0,0 +1,7 @@
+public class SendMailVM
+{
+    public string[] Emails { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public bool isHtml { get; set; }
+}
